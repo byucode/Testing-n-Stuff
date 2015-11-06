7f60fa70b43feff36736c1cf6c5fe94c4caeb357
byucode <scott_evans@byu.edu>
2015-11-06 08:51:48 -0700

    changes

--- Sandbox/src/sandbox/Elmo.js
+++ Sandbox/src/sandbox/Elmo.js
@@ -1,3 +1,5 @@
+console.log("Hello World");
+console.log("Hello World again");
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
